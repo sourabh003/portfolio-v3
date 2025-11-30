bb94b6f0213fe544f73ca14d8797bf8664749270
Sourabh Choudhary <csourabh003@gmail.com>
2025-11-30 11:14:10 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// serve static files from public/
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
